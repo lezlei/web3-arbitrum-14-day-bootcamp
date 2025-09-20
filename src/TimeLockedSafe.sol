e13431b3774b7fcf8cdb0d67951a24532d2f8ffb
lezlei <27leetyler@gmail.com>
2025-09-20 21:17:42 +0800

    feat: complete core logic and happy path tests

--- src/TimeLockedSafe.sol
+++ src/TimeLockedSafe.sol
@@ -1,4 +1,40 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract Safe {}
+contract TimeLockedSafe {
+    // A struct to hold the details of a single user's safe
+    struct Safe {
+        uint256 amount;
+        uint256 unlockTime;
+    }
+
+    // A mapping where:
+    // - The key is a user's address
+    // - The value is their personal Lock struct
+    mapping(address => Safe) public safes;
+
+    // Events
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdrawal(address indexed recipient, uint256 amount);
+
+    // Functions
+
+    // Accepts deposit then creates a safe for the user
+    function deposit(uint256 _duration) external payable {
+        require(msg.value > 0);
+        require(safes[msg.sender].amount == 0, "safe already active");
+        safes[msg.sender] = Safe({amount: msg.value, unlockTime: block.timestamp + _duration});
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    // Sends a user's balance back to the user if the lock time is over, then deletes their safe
+    function withdraw() external {
+        require(safes[msg.sender].amount > 0, "no active safe");
+        require(block.timestamp >= safes[msg.sender].unlockTime, "safe is still locked");
+        uint256 withdrawAmount = safes[msg.sender].amount;
+        (bool success,) = msg.sender.call{value: withdrawAmount}("");
+        require(success, "Transfer failed");
+        delete safes[msg.sender];
+        emit Withdrawal(msg.sender, withdrawAmount);
+    }
+}
--- test/TimeLockedSafe.t.sol
+++ test/TimeLockedSafe.t.sol
@@ -1,13 +1,48 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
 import {Test} from "forge-std/Test.sol";
-import {Safe} from "../src/TimeLockedSafe.sol";
+import {TimeLockedSafe} from "../src/TimeLockedSafe.sol";
 
 contract SafeTest is Test {
-    Safe public safe;
+    TimeLockedSafe public safe;
 
     function setUp() public {
-        safe = new Safe();
+        safe = new TimeLockedSafe();
+    }
+
+    function testAnyoneCanDeposit() public {
+        hoax(address(1), 100);
+        safe.deposit{value: 100}(120);
+        assertEq(address(safe).balance, 100, "deposit failed 1");
+        hoax(address(2984375), 100);
+        safe.deposit{value: 100}(120);
+        assertEq(address(safe).balance, 200, "deposit failed 2");
+    }
+
+    function testSafeCreated() public {
+        hoax(address(1), 100);
+        uint256 time = block.timestamp;
+        safe.deposit{value: 100}(10);
+        (uint256 lockedAmount, uint256 unlockTime) = safe.safes(address(1));
+        assertEq(lockedAmount, 100, "deposit failed 3");
+        assertEq(unlockTime, time + 10, "deposit failed 4");
+    }
+
+    function testCanUserWithdraw() public {
+        hoax(address(1), 100);
+        uint256 time = block.timestamp;
+        safe.deposit{value: 100}(10);
+        vm.warp(time + 10);
+        uint256 usercurrentbal = address(1).balance;
+        uint256 contractcurrentbal = address(safe).balance;
+
+        vm.prank(address(1));
+        safe.withdraw();
+
+        assertEq(usercurrentbal, address(1).balance - 100, "withdrawal failed 1");
+        assertEq(contractcurrentbal, address(safe).balance + 100, "withdrawal failed 2");
+        (uint256 amtleft,) = safe.safes(address(1));
+        assertEq(amtleft, 0, "withdrawal failed 3");
     }
 }
